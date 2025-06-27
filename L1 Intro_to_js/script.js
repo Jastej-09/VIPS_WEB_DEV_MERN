d92043deb3d38072ece304fc6d71e0a81784b56c
Jastej Singh <pvsjas4609@gmail.com>
2025-06-27 16:22:56 +0530

    L1_INTRO_TO_JS

---
+++ L1 Intro_to_js/script.js
@@ -0,0 +1,134 @@
+// console.log("ATTENDANCE")
+// console.log("CLASS LECTURE");
+// console.log("LUNCH");
+// console.log("class end");
+
+// How we declare variable
+
+// let a=10;
+// console.log(a);
+// a=30;
+// console.log(a);
+
+// const b = 20;
+// console.log(b);
+// b=40;
+
+// function sumoftwo(param1,param2){
+//     var answer = param1+param2;
+//     return answer
+// }
+// var arg1=10;
+// var arg2=20;
+// console.log(sumoftwo(arg1,arg2));
+
+//how java script code runs
+
+// let a=10;
+// let b=20;
+// console.log(a);
+// function sumoftwo(param1,param2){
+//     let answer = param1+param2;
+//     return answer
+// }
+// let arg1=10;
+// let arg2=20;
+// console.log(sumoftwo(arg1,arg2));
+// console.log(b);
+
+// console.log(a);
+
+// var a=20;
+// var b= 30;
+
+// console.log(b);
+
+// console.log(aa);
+
+// let aa=200;
+// let bb=400;
+
+// console.log(bb);
+
+// var a=100;
+// 
+// console.log(a);
+
+// function firstfun(){
+//     var a=20;
+//     var b=30;
+//     console.log("HELLO I AM FIRST FUNCTION");
+//     function secFun(){
+//         var c=50;
+//         console.log("HELLO I AM SECOND FUNCTION");
+//         function thirdfun(){
+//             var d= 80;
+//                 console.log("HELLO I AM THIRD FUNCTION");
+
+//             return a+b+c+d;
+
+//         }
+//        return thirdfun();
+//     }return secFun();
+// }
+// console.log(firstfun());
+
+
+// function firstfun(){
+//     let a=20;
+//     let b=30;
+//     console.log("HELLO I AM FIRST FUNCTION");
+//     function secFun(){
+//         let c=50;
+//         console.log("HELLO I AM SECOND FUNCTION");
+//         function thirdfun(){
+//             let d= 80;
+//                 console.log("HELLO I AM THIRD FUNCTION");
+
+//             return a+b+c+d;
+
+//         }
+//        return thirdfun();
+//     }return secFun();
+// }
+// console.log(firstfun());
+
+// set timeout &setinterval
+
+// setTimeout(function (){
+//     console.log("HELLO I AM SETTIMEOUT");
+// },5000);
+
+// setInterval(function (){
+//     console.log("HELLO I AM SETINTERVAL");
+
+// },2000);
+
+// for(var i=0;i<=5;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+
+//     },2000);
+// }
+
+// for(let i=0;i<=5;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+
+//     },2000);
+// }
+
+// const intervalId = setInterval(() => {
+//   console.log('Running...');
+// }, 1000);
+
+// // To stop the interval after 5 seconds:
+// setTimeout(() => {
+//   clearInterval(intervalId);
+//   console.log('Interval stopped.');
+// }, 5000);
+
+
+
+
+
