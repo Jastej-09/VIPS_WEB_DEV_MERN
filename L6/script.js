23e5c5c4eec8a197541641bcb12b20830dcfdf86
Jastej Singh <pvsjas4609@gmail.com>
2025-07-16 11:24:49 +0530

    L6

---
+++ L6/script.js
@@ -0,0 +1,143 @@
+// console.log(1+2);
+
+// let str = "hello world";
+// let a=10;
+// console.log(str)
+
+ //template literals
+
+// let str2 = `helloworld${a}`
+// console.log(str2);
+
+// let colors = ['red','blue','green','grey']
+// colors.splice(1,1)
+// let str='hello bum ahh world'
+// let arr = str.split(" ")
+// console.log(str.split(" "))
+// console.log(arr.join(" ")
+// )
+
+// let arr= [10,20,30,40,50]
+// let newarr= arr.map((val ,index)=>{
+//     return val += 5;
+// })
+// console.log(newarr);
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+// console.log(arr.filter((val,index)=>{
+//     if(val%2!=0){
+//         return val
+//     }
+// }))
+
+// let nigger=arr.filter((val,index)=>{
+//     if(val%2==0){
+//         return val
+//     }
+// })
+// console.log(nigger);
+
+
+// let newarr=arr.reduce((prev,val,index , arrr)=>{
+//         console.log("prev ---->",prev)
+//         console.log("val ---->",val)
+//         console.log("index ---->",index)
+//         console.log(arrr);
+//         return prev+val
+
+// })
+
+// console.log(newarr)
+
+let obj = {
+    Name :"Jastej",
+    class:"barwi",
+    age:22,
+    address :{
+        city: "delhi",
+        country: "india"
+    },
+    fun :function(){
+        console.log("Hello everynyan from "+ this.Name);
+        
+    }
+}
+
+obj.fun();
+// console.log(...arr);
+
+// newarr=(...arr);
+
+// console.log(newarr);
+
+
+// obj.age=25;
+// console.log(obj.age);
+
+// obj.college="VIPS"
+
+// console.log(obj.college);
+
+
+
+//DATE
+
+// let date = new Date();
+// console.log(date.getFullYear());
+
+// let float = 12.5890;
+// console.log(Math.floor(float));
+
+// console.log(Math.ceil(float));
+
+// let ran = Math.random();
+// console.log(ran);
+
+
+//OBJECT DESTRUCTING
+
+// let person = {
+//     firstn: "raj",
+//     lastname: "gupta",
+//     age:22,
+// }
+
+// let narr =['c','b','a','d']
+// for(const element of narr){
+//     console.log(element);
+// }
+
+// let arr=[1,2,3,4,5,6,7,8,9,10]
+
+// for(const elem of arr){
+//     console.log(elem)
+// }
+
+// for(key in person){
+//     console.log(key+"-->"+person[key]);
+
+// }
+
+// function one(){
+//     console.log("first Function")
+// }
+// function second(chacha){
+//     chacha();
+//     console.log("second Function")
+// }
+// second(one);
+
+// learn IIfe
+
+// setTimeout(()=>{
+//     console.log("sup my nigga");
+// },3000)
+// console.log("hello homie");
+// console.log("you been good");
+
+// setInterval(() => {
+//     console.log("ola espaneol")
+// }, 2000);
+
+
+
