391ba58245c9b5a51411ca12c103d3dbeb411384
Jastej Singh <pvsjas4609@gmail.com>
2025-07-16 11:24:20 +0530

    L5

---
+++ L5/script.js
@@ -0,0 +1,16 @@
+var a= 10;
+console.log(a);
+
+var sum=(a,b)=>{
+    return a+b;
+}
+sum(10,20);
+var c=sum(20,30);
+console.log(c);
+
+let arr = [10,20,30,40];
+// console.log(arr);
+let newarr= arr;
+newarr.push(50);
+console.log(newarr);
+
